5c0b6f94f6a2ee539f80fd103511e4d8815d7473
PATWARY <md.hasan.69q@gmail.com>
2022-10-21 00:26:35 +0600

    Adding Error Handling

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -2,7 +2,6 @@ import classes from "./comment-list.module.css";
 
 function CommentList(props) {
   const { comments } = props;
-  console.log(comments);
   return (
     <ul className={classes.comments}>
       {/* Render list of comments - fetched from API */}
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,18 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://patwary:mhpatwary72@cluster0.og93a4g.mongodb.net/?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+async function insertedDocument(client, document) {
+  const db = client.db("events");
+  await db.collection("emails").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -9,14 +22,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://patwary:mhpatwary72@cluster0.og93a4g.mongodb.net/?retryWrites=true&w=majority"
-    );
+    let client;
 
-    const db = client.db("events");
-    await db.collection("emails").insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (err) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertedDocument(client, { email: userEmail });
+      client.close();
+    } catch (err) {
+      res.status(500).json({ message: "Inserting Data failed!" });
+      return;
+    }
 
     res.status(201).json({ message: "Signed Up!" });
   }
